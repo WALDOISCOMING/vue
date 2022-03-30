da9bde0abb0ff2257598155b97c1108d0e3f4f0a
kilkyungwan <forallabouttest@gmail.com>
2022-03-30 13:17:06 +0900

    add profile page

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from '../src/App.vue'
 import MainPage from '../src/pages/MainPage.vue'
 import LoginPage from '../src/pages/LoginPage.vue'
+import ProfilePage from './pages/ProFilePage.vue'
 
 import VueRouter from 'vue-router';
 
@@ -11,6 +12,7 @@ import VueRouter from 'vue-router';
 const routes = [
   { path: "/home", component: MainPage ,name: "home", },
   { path: "/login", component: LoginPage ,name: "login", },
+  { path: "/profile", component: ProfilePage ,name: "profile", },
   { path: "/", component: MainPage ,name: "home", }
 ];
 
