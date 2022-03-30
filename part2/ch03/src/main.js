cf12ec66c8e90c422554b18e57c75f0d5549d9e2
kilkyungwan <forallabouttest@gmail.com>
2022-03-30 13:31:51 +0900

    ♻️ refack routes #5

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -1,28 +1,17 @@
 import Vue from 'vue'
 import App from '../src/App.vue'
-import MainPage from '../src/pages/MainPage.vue'
-import LoginPage from '../src/pages/LoginPage.vue'
-import ProfilePage from './pages/ProFilePage.vue'
+import router from './routes'
 
 import VueRouter from 'vue-router';
 
 // 1. Define route components.
 
 // 2. Define some routes
-const routes = [
-  { path: "/home", component: MainPage ,name: "home", },
-  { path: "/login", component: LoginPage ,name: "login", },
-  { path: "/profile", component: ProfilePage ,name: "profile", },
-  { path: "/", component: MainPage ,name: "home", }
-];
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
 // 3. Create the router instance and pass the `routes` option
-const router = new VueRouter({
-  routes
-});
 
 /*const app = */
 new Vue({
