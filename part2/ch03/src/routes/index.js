8dc515aae1badece85cd460c8d4c4c6497466ada
kilkyungwan <forallabouttest@gmail.com>
2022-03-30 17:04:36 +0900

    add api client

--- part2/ch03/src/routes/index.js
+++ part2/ch03/src/routes/index.js
@@ -8,7 +8,17 @@ import VueRouter from 'vue-router';
 const routes = [
   { path: "/home", component: MainPage ,name: "home", },
   { path: "/login", component: LoginPage ,name: "login", },
-  { path: "/profile", component: ProfilePage ,name: "profile", },
+  {
+    path: '/profile',
+    component: ProfilePage,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('token')) {
+        next();
+      } else {
+        next('/login');
+      }
+    },
+  },
   { path: "/", component: MainPage ,name: "home", }
 ];
 
