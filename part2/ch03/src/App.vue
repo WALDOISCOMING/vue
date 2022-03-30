7cc90c76433bc0c7af2c415cbf9f26393a9395b3
kilkyungwan <forallabouttest@gmail.com>
2022-03-30 10:56:31 +0900

    ✨ simple comp #4

---
+++ part2/ch03/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-link to="/home">Go to Home</router-link> |
+    <router-link to="/login">Go to Login</router-link> |
+    <SimpleComp></SimpleComp>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import SimpleComp from './components/SimpleComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ part2/ch03/src/components/SimpleComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Simple Comp</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimpleComp',
+  props: {
+    msg: String
+  },
+  data : function(){
+    return {}
+  }
+}
+</script>
